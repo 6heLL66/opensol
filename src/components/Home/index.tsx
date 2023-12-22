ec94438ccb9b2860700907b01265fa0cf203f9ef
getlost <sseniknik@gmail.com>
2023-12-22 04:00:47 +0300

    fix: fix

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,17 @@
 import { SocialLinks } from '../SocialLinks'
 
+import { Slide, Fade } from 'react-awesome-reveal'
+
 import styles from './styles.module.scss'
 
 export const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>Open Sol Project</h1>
-      <div className={styles.links}></div>
-      <SocialLinks />
-    </div>
+    <Fade delay={300} duration={1300}>
+      <div className={styles.container}>
+        <h1 className={styles.header}>Open Sol Project</h1>
+        <div className={styles.links}></div>
+        <SocialLinks />
+      </div>
+    </Fade>
   )
 }
